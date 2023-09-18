1899d98683d08d8cc30892015e1e105069f0067c
florius88 <florsantosbueno@gmail.com>
2023-09-18 19:43:09 +0200

    33. Creando nuestro primer modulo

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppRoutingModule } from '../app-routing.module';
+import { SharedModule } from '../shared/shared.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+import { ProgressComponent } from './progress/progress.component';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    Grafica1Component,
+    PagesComponent,
+    ProgressComponent,
+  ],
+  exports: [
+    DashboardComponent,
+    Grafica1Component,
+    PagesComponent,
+    ProgressComponent,
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    SharedModule,
+  ]
+})
+export class PagesModule { }
