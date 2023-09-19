d2fb62d080acfcce5a82de1544208a8048e929d6
florius88 <florsantosbueno@gmail.com>
2023-09-19 10:40:47 +0200

    37. AuthRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 
+import { AuthRoutingModule } from './auth/auth.routing';
 import { PagesRoutingModule } from './pages/pages.routing';
-
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 const routes: Routes = [
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
+
+  // path: '/dashboard'   - PagesRouting
+  // path: '/auth'        - AuthRouting
+
   { path: '**', component: NopagefoundComponent },
 ]
 
@@ -20,6 +20,7 @@ const routes: Routes = [
   declarations: [],
   imports: [
     RouterModule.forRoot(routes),
+    AuthRoutingModule,
     PagesRoutingModule,
   ],
   exports: [RouterModule]
