d7ab5bd7123ce98dbc5487a97d787f0d7a3df52c
florius88 <florsantosbueno@gmail.com>
2023-09-19 13:08:24 +0200

    48. Uso de atributos personalizados

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from '../app-routing.module';
 import { SharedModule } from '../shared/shared.module';
@@ -26,6 +27,7 @@ import { ProgressComponent } from './progress/progress.component';
   ],
   imports: [
     CommonModule,
+    FormsModule,
     AppRoutingModule,
     SharedModule,
   ]
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styleUrls: [ './progress.component.css' ],
+  styleUrls: ['./progress.component.css'],
 })
 export class ProgressComponent {
+
+  progreso: number = 10;
+
+  get getPorcentaje() {
+    return `${this.progreso}%`
+  }
+
+  cambiarValor(valor: number) {
+    if (this.progreso >= 100 && valor >= 0) {
+      return this.progreso = 100
+    }
+
+    if (this.progreso <= 100 && valor < 0) {
+      return this.progreso = 0
+    }
+
+    return this.progreso = this.progreso + valor
+  }
 
 }
