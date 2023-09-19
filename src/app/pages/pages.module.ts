4c510216331f534eb816ccdb9428792752c3ca33
florius88 <florsantosbueno@gmail.com>
2023-09-19 13:43:06 +0200

    49. Crear componente incrementador

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from '../app-routing.module';
+import { ComponentsModule } from '../components/components.module';
 import { SharedModule } from '../shared/shared.module';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -29,6 +30,7 @@ import { ProgressComponent } from './progress/progress.component';
     CommonModule,
     FormsModule,
     AppRoutingModule,
+    ComponentsModule,
     SharedModule,
   ]
 })
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -7,22 +7,4 @@ import { Component } from '@angular/core';
 })
 export class ProgressComponent {
 
-  progreso: number = 10;
-
-  get getPorcentaje() {
-    return `${this.progreso}%`
-  }
-
-  cambiarValor(valor: number) {
-    if (this.progreso >= 100 && valor >= 0) {
-      return this.progreso = 100
-    }
-
-    if (this.progreso <= 100 && valor < 0) {
-      return this.progreso = 0
-    }
-
-    return this.progreso = this.progreso + valor
-  }
-
 }
