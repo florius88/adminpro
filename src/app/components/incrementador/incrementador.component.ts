b933cfaa17b391a3e19048c5c5ec9fd3711de26b
florius88 <florsantosbueno@gmail.com>
2023-09-20 13:35:53 +0200

    50. Input

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -8,11 +8,7 @@ import { Component } from '@angular/core';
 })
 export class IncrementadorComponent {
 
-  progreso: number = 10;
-
-  get getPorcentaje() {
-    return `${this.progreso}%`
-  }
+  @Input('valor') progreso: number = 10;
 
   cambiarValor(valor: number) {
     if (this.progreso >= 100 && valor >= 0) {
